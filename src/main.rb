5322f2ed61faec4ce75d3821ec99cd7f2fa0d5f1
Nicholas Skinsacos <nskins@umich.edu>
2022-01-15 16:34:46 -0500

    Drop in existing Ayara files

--- src/main.rb
+++ src/main.rb
@@ -1,37 +1,18 @@
-require 'goby'
+require_relative "../lib/driver.rb"
+require_relative "../lib/Battle/BattleCommand/use.rb"
+require_relative "Battle/BattleCommand/Attack/punch.rb"
+require_relative "../lib/Entity/player.rb"
+require_relative "Map/Map/ayara.rb"
+require_relative "Story/intro.rb"
 
-include Goby
-
-require_relative 'map/farm.rb'
-
-# Set this to true in order to use BGM.
-Music::set_playback(false)
-
-# By default, we've included no music files.
-# The Music module also includes a function
-# to change the music-playing program.
-
-# Clear the terminal.
+print_intro
 system("clear")
 
-# Allow the player to load an existing game.
-if File.exists?("player.yaml")
-  print "Load the saved file?: "
-  input = player_input
-  if input.is_positive?
-    player = load_game("player.yaml")
-  end
-end
-
-# No load? Create a new player.
-if player.nil?
-  # A Location specifies the Map and (y,x) coordinates of a Player.
-  home = Location.new(Farm.new, C[1, 1])
-
-  # Use the Player constructor to set the
-  # location, stats, gold, inventory, and more.
-  player = Player.new(location: home)
-
-end
-
+player = Player.new(
+                    max_hp: 15,
+                    attack: 3,
+                    map: Ayara.new,
+                    location: Couple.new(11,4),
+                    battle_commands: [Punch.new, Use.new]
+                    )
 run_driver(player)
