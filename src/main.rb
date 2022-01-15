f8ac813c73693dfff174b563f659bad04cf84d99
Nicholas Skinsacos <nskins@umich.edu>
2022-01-15 17:25:53 -0500

    Implement loading saved games

--- src/main.rb
+++ src/main.rb
@@ -6,16 +6,42 @@ require_relative "battle/battle_command/attack/punch.rb"
 require_relative "map/map/ayara.rb"
 require_relative "story/intro.rb"
 
-print_intro
+# Set this to true in order to use BGM.
+Music::set_playback(false)
+
+# By default, we've included no music files.
+# The Music module also includes a function
+# to change the music-playing program.
+
 system("clear")
 
-player = Player.new(
+# Allow the player to load an existing game.
+if File.exists?("player.yaml")
+  print "Load the saved file?: "
+  input = player_input
+  if input.is_positive?
+    player = load_game("player.yaml")
+  end
+end
+
+# No load? Create a new player.
+if player.nil?
+  print_intro
+
+  # A Location specifies the Map and (y,x) coordinates of a Player.
+  home = Location.new(Ayara.new, C[11,4])
+
+  # Use the Player constructor to set the
+  # location, stats, gold, inventory, and more.
+  player = Player.new(
                     stats: {
                       max_hp: 15,
                       attack: 3
                     },
-                    location: Location.new(Ayara.new, C[11,4]),
-                    respawn_location: Location.new(Ayara.new, C[11,4]),
+                    location: home,
                     battle_commands: [Punch.new, Use.new]
                     )
+
+end
+
 run_driver(player)
--- src/story/intro.rb
+++ src/story/intro.rb
@@ -1,18 +1,18 @@
 def print_intro
   system("clear")
-  
-  print "Do you know how to play (y/n)?: "
+
+  print "Do you know how to play?: "
   input = gets.chomp
   print "\n"
-  
-  return if input != 'n'
-  
+
+  return if input.is_positive?
+
   puts "First things first - interaction"
   puts "is handled by commands. Here are some"
   print "of the most important ones:\n\n"
-  
+
   sleep(4)
-  
+
   print "     Command        Purpose\n\n"
   puts  "      w (↑)"
   puts  "a (←) s (↓) d (→)   Movement"
@@ -21,16 +21,16 @@ def print_intro
   puts  "      help          See ALL commands"
   puts  "       inv          Check inventory"
   print "   use [item]       Use an item\n\n"
-  
+
   print "Press enter to continue..."
   gets
-  
+
   puts "\nAlso, it's necessary to stress that"
   print "exploration is key.\n\n"
-  
+
   sleep(2)
-  
+
   puts "Try out different things!"
-  
+
   sleep(2)
 end
